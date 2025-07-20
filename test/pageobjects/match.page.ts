6856628b59e1de95d54daf8387ab8c1b07055733
Constantine Caviris <caviris@gmail.com>
2025-07-19 21:26:53 -0400

    Updated match.page.ts to get starting players, bench players, and managers

--- test/pageobjects/match.page.ts
+++ test/pageobjects/match.page.ts
@@ -7,10 +7,17 @@ class MatchPage extends Page {
     /**
      * define selectors using getter methods
      */
-    public get playerNames () {
-        return $$('.mls-o-pitch__foreground  .mls-o-player-block__player-name');
+    public get startingPlayers () {
+        return $$('.mls-o-pitch__foreground .mls-o-player-block__player-name');
     }
 
+    public get benchPlayers () {
+        return $$('.mls-c-lineups__substitutions .mls-o-player-block__player-name')
+    }
+
+    public get managers () {
+        return $$('.mls-c-lineups__managers .mls-o-player-block__player-name')
+    }
 
     /**
      * overwrite specific options to adapt it to page object
@@ -19,8 +26,23 @@ class MatchPage extends Page {
         return super.open(urlPath);
     }
 
-    public async getPlayerNames (){
-        const playerNames = await this.playerNames;
+    public async getPlayerNames (playerType = 'starting'){
+        let playerNames;
+        switch (playerType) {
+            case 'starting':
+                playerNames = await this.startingPlayers;
+                break;
+            case 'bench':
+                playerNames = await this.benchPlayers;
+                break
+            case 'manager':
+                playerNames = await this.managers;
+                break;
+            default:
+                console.log(`Invalid input of ${playerType} in getPlayerNames function. Valid inputs inclue 'starting', 'bench', or 'manager'`)
+                return null;
+        }
+        
         const playerNameStrings = [];
         await playerNames.forEach(async (playerName) => {
             const name = await playerName.getText();
