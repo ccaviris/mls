9fcd77f64a22fd4d565108411fc4be0d32d33771
Constantine Caviris <caviris@gmail.com>
2025-07-19 20:48:37 -0400

    Updated test.ts to use array camparrer and validate with chai assertions

--- test/specs/test.ts
+++ test/specs/test.ts
@@ -1,15 +1,18 @@
 //import { expect } from '@wdio/globals'
+import { expect } from 'chai';
 import matchPage from '../pageobjects/match.page.ts'
-import { feedParser } from '../../util/feedParser.ts'
+import feedParser from '../../util/feedParser.ts'
+import compareArrays from '../../util/compareArrays.ts'
 
 describe('Validate match rosters ', () => {
     it('should validate starting rosters', async () => {
         await matchPage.open()
-        const observedPlayerNames = await matchPage.getPlayerNames();
-        console.log('Observed ' + observedPlayerNames.toString());
-        const parser = new feedParser();  
-        const expectedPlayers = parser.getStartingPlayers();
+        const observedPlayers = await matchPage.getPlayerNames();
+        console.log('Observed ' + observedPlayers.toString());
+        const expectedPlayers = feedParser.getStartingPlayers();
         console.log('Expected ' + expectedPlayers.toString());
+
+        expect(compareArrays.verifyIfEqual(observedPlayers, expectedPlayers)).to.be.true;
     })
 })
 
