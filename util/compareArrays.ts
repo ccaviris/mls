3b6673f23dd34698a4d5a714de59437e2f749d00
Constantine Caviris <caviris@gmail.com>
2025-07-20 23:07:31 -0400

    Improved compareArrays class to print discrepancies between the two arrays

--- util/compareArrays.ts
+++ util/compareArrays.ts
@@ -1,22 +1,35 @@
 class compareArrays {
 
 /**
- * A quick and basic comparison of two array
+ * A comparison of two array that prints discrepant values
  * @param firstArray The first array to be comparison
- * @param secondArray THe second array for comparison
+ * @param secondArray The second array for comparison
  * @returns True if the two arrays contain the same values and false otherwise
  */
   public verifyIfEqual(firstArray:string[] = [], secondArray:string[] = []) {
-    //This is functional a bit simple and crude, sorting and joining each array and comparing them as string
-    //One improvement would be to compare all of the elements of the two arrays and print out all of the values
-    //of the first array not present in the second and vice versa. Annother improvment would be to return a key pair array
-    //with a boolean value to denote if they match or do not and a string with a summary of the discrepent values
+    //One  improvment would be to return a key pair array
+    //with a boolean value to denote if they match or do not and a 
+    // string with a summary of the discrepent values
     //This string could be used in the error message within the test.
-    if(firstArray.sort().join(',')=== secondArray.sort().join(',')){
-      return true
+    let arraysMatch = true;
+    for(const value of firstArray){
+      const index = secondArray.indexOf(value) 
+      console.log(index);
+      if(index === -1){
+        arraysMatch = false;
+        console.log(`The vale ${value} was found in the first array but not the second`)
+      } else {
+        //Remove the found element to save time later
+        secondArray.splice(index, 1)
+        console.log(secondArray.join(', '))
+      }
     }
-    else{
-      return false
+    //If we remove all elements from the first array found in the second array, what is left is values unique to the second array
+    //Or valyes that appear more times in the second array than the first (example [1, 2, 3] and [1, 2, 3, 3])
+    if(secondArray.length > 0){
+      arraysMatch = false;
+      console.log(`The second array contains the following values not found in the first array ${secondArray.join(', ')}`)
     }
+    return arraysMatch;
   }
 } export default new compareArrays();
